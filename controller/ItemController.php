b322eb08ae2862941d68415ad0d03474614b8a97
npovsic <npovsic@gmail.com>
2017-01-09 22:05:47 +0100

    added database access with PDO (localhost)

--- controller/ItemController.php
+++ controller/ItemController.php
@@ -4,7 +4,21 @@ require_once("View.php");
 
 class ItemController {
 
-    public static function index() {
+    public static function index($db) {
+//        $sql = "CREATE TABLE MyGuests (
+//            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+//            firstname VARCHAR(30) NOT NULL,
+//            lastname VARCHAR(30) NOT NULL,
+//            email VARCHAR(50),
+//            reg_date TIMESTAMP
+//            )";
+//
+//        if ($db->query($sql) === TRUE) {
+//            echo "Table MyGuests created successfully";
+//        } else {
+//            echo "Error creating table: " . $db->error;
+//        }
+
         echo View::render("view/layout.php");
     }
 
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 session_start();
 
 require_once("controller/ItemController.php");
+require_once("sql/InitDB.php");
 
 define("BASE_URL", $_SERVER["SCRIPT_NAME"] . "/");
 define("IMAGES_URL", rtrim($_SERVER["SCRIPT_NAME"], "index.php") . "static/images/");
@@ -13,7 +14,8 @@ $path = isset($_SERVER["PATH_INFO"]) ? trim($_SERVER["PATH_INFO"], "/") : "";
 // ROUTER: defines mapping between URLS and controllers
 $urls = [
     "" => function () {
-        ItemController::index();
+        $db = InitDB::getInstance();
+        ItemController::index($db);
     },
     "login" => function() {
         ItemController::login();

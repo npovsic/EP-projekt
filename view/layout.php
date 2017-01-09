191f423e65186066deb52d90a5e97429bd6f2b32
npovsic <npovsic@gmail.com>
2017-01-09 19:39:04 +0100

    added two more items for display

--- view/layout.php
+++ view/layout.php
@@ -31,6 +31,44 @@
                     </article>
                 </div>
 
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <article class="material-card Blue-Grey">
+                        <h2>
+                            <span>Battery Whey Protein</span>
+                            <strong class="info-left">2000g</strong>
+                            <strong class="info-right">20e</strong>
+                        </h2>
+
+                        <div class="mc-content">
+                            <div class="img-container">
+                                <img class="img-responsive" src="<?php echo IMAGES_URL . "battery_whey_protein.jpg" ?>">
+                            </div>
+                        </div>
+                        <a href="" class="mc-btn-action">
+                            <i class="fa fa-info-circle"></i>
+                        </a>
+                    </article>
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <article class="material-card Blue-Grey">
+                        <h2>
+                            <span>Battery Whey Protein</span>
+                            <strong class="info-left">2000g</strong>
+                            <strong class="info-right">20e</strong>
+                        </h2>
+
+                        <div class="mc-content">
+                            <div class="img-container">
+                                <img class="img-responsive" src="<?php echo IMAGES_URL . "battery_whey_protein.jpg" ?>">
+                            </div>
+                        </div>
+                        <a href="" class="mc-btn-action">
+                            <i class="fa fa-info-circle"></i>
+                        </a>
+                    </article>
+                </div>
+
             </div>
         </div>
 

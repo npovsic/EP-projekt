4f33853974a4d053fec59e2dfd404368cf3dbb89
npovsic <npovsic@gmail.com>
2018-01-13 16:03:17 +0100

    Small bugfix.

--- view/demo_captcha.php
+++ view/demo_captcha.php
@@ -7,8 +7,7 @@ $bg = imagecolorallocate($im, 22, 86, 165); //background color blue
 $fg = imagecolorallocate($im, 255, 255, 255);//text color white
 imagefill($im, 0, 0, $bg);
 imagestring($im, 5, 5, 5,  $code, $fg);
-header("Cache-Control: no-cache, must-revalidate");
-header('Content-type: image/png');
+//header("Cache-Control: no-cache, must-revalidate");
+//header('Content-Type: image/png');
 imagepng($im);
 imagedestroy($im);
-?>
